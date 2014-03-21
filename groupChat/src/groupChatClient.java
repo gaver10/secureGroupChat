4d4703e9b29294c6b54f9b26f611585cc40ddc6b
Ken Fox <kwf26@drexel.edu>
2014-03-21 06:05:10 -0400

    basic chatClient to wrap the gui or be absorbed by it

---
+++ groupChat/src/groupChatClient.java
@@ -0,0 +1,32 @@
+/**
+ * CS475 Secure Group Chat Server
+ * Ken Fox, Gavin Rapp, Andrea Pavia
+ * basic chat client program 
+ * will move this code into gui or reference gui from here.
+ * 
+ */
+
+/**
+ * @author kenfox
+ *
+ */
+public class groupChatClient {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * Sends a control message from the chat client to the server
+	 * for instance quit, 
+	 */
+	public void sendMessageToServer() {
+		
+	}
+	
+	
+}
